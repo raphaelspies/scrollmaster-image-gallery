f138fb1b0da0fc26ac9580243d92d600cfeaaca5
Raphael <raphspies@gmail.com>
2021-10-05 17:30:15 -0600

    updates that should have been in first push

---
+++ pages/ScrollBox.js
@@ -0,0 +1,9 @@
+
+export default function ScrollBox (props) {
+
+  return (
+    <div>
+      This is the Scroll Box
+    </div>
+  )
+}
---
+++ public/env.js
@@ -0,0 +1,3 @@
+window.env = {
+  "API_KEY": "Y4s7xLziwOnWKQy4330wQT3PDCfrJTA3Z7hswMhK0Cw"
+};
