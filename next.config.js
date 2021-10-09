9ce0815c3b1a8997f70debe232190995ff0c5dc8
raphspies <raphspies@gmail.com>
2021-10-09 16:54:46 -0700

    commented out env variables in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   reactStrictMode: true,
-  env: {
-    API_KEY: 'your_api_key',
-  },
+  // env: {
+  //   API_KEY: 'your_api_key',
+  // },
   images: {
     domains: ['images.unsplash.com'],
   },
