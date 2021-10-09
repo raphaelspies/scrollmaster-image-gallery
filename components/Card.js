79821b80faace7770b7262ae8377f38c881f20cd
raphspies <raphspies@gmail.com>
2021-10-08 23:03:29 -0700

    implemented modal, styled text on modal

--- components/Card.js
+++ components/Card.js
@@ -1,25 +1,67 @@
+import { useState } from 'react'
 import Image from 'next/image'
 import styles from '../styles/Card.module.css'
+import { DateTime } from 'luxon';
+
 
 export default function Card (props) {
+ const [isModal, setIsModal] = useState(false)
+
+ const openModal = function(e) {
+  setIsModal(prevState => (!prevState))
+}
+
+
  const {
   endOfListRef,
   info: {
     urls,
     alt_description,
     width,
-    height
+    height,
+    created_at,
+    user: {
+      name,
+      bio,
+      links: {
+        portfolio,
+      }
+    }
   },
  } = props
 
+ const CreationDate = DateTime.fromISO(created_at).toLocaleString();
   return (
-    <div ref={endOfListRef} className={styles.Card}>
-      <Image
-        src={urls.small}
-        alt={alt_description}
-        width={600}
-        height={height / (width/600)}
-      />
-    </div>
+    !isModal ? (
+      <div ref={endOfListRef} className={styles.Card} onClick={openModal}>
+        <Image
+          src={urls.small}
+          alt={alt_description}
+          width={600}
+          height={height / (width/600)}
+        />
+      </div>)
+    : (
+      <div ref={endOfListRef} className={styles.modal} onClick={openModal}>
+        <div className={styles.modalContent}>
+          <div className={styles.modalHorizontal}>
+          <Image
+            src={urls.small}
+            alt={alt_description}
+            width={600}
+            height={height / (width/600)}
+          />
+          <div className={styles.modalText}>
+            <h2>{alt_description}</h2>
+            <ul>
+              <li><b> Author: </b> {name}</li>
+              <li><b> Author Bio: </b> {bio}</li>
+              <li><b> Date Created: </b> {CreationDate}</li>
+            </ul>
+          </div>
+        </div>
+        </div>
+      </div>)
+
   )
 }
--- components/ScrollBox.js
+++ components/ScrollBox.js
@@ -5,15 +5,28 @@ import Card from './Card'
 export default function ScrollableList(props) {
   const { onScroll, endOfList, endOfListRef, images, view} = props;
 
+  const openModal = function(e) {
+    console.log("e.target")
+  }
+
   return (
+    <div>
+    <div className={styles.modal}>
+      <div className={styles.modalContent}>
+        <span className={styles.closeModal}>
+          <p>text here</p>
+        </span>
+      </div>
+    </div>
     <div className={ view ? styles.column : styles.grid}>
       {images.map((image, index) => {
         if (index === images.length -1) {
-          return (<Card key={image.id} info={image} endOfListRef={endOfListRef}/>)
+          return (<Card key={image.id} info={image} onClick={()=> console.log("hello")} endOfListRef={endOfListRef}/>)
         } else {
           return (<Card key={image.id} info={image} />)
         }
       })}
+    </div>
     </div>
   );
 
